07f859febeaedd5735b7cb5ad1dd24bb639e1bc2
NAZIYA8 <syeda.naziya896@gmail.com>
2021-06-11 18:42:26 +0530

    Refactor: All 3  to one generic method

--- src/com/bridgelabz/Maximum.java
+++ src/com/bridgelabz/Maximum.java
@@ -1,64 +1,28 @@
 package com.bridgelabz;
 
-import java.lang.Float;
-import java.lang.Integer;
-
 public class Maximum implements Comparable {
 
-    /**
-     * @param x integer1
-     * @param y integer2
-     * @param z integer3
-     * @return maximum of 3 integers
-     */
-    public static Integer maximumOfIntegers(Integer x, Integer y, Integer z) {
-        Integer max = x;
+    public static <T extends Comparable<T>> T maximum(T x, T y, T z) {
+        T max = x;
+
         if (y.compareTo(max) > 0)
             max = y;
         if (z.compareTo(max) > 0)
             max = z;
         return max;
-    }
 
-    /**
-     * @param a float number1
-     * @param b float number2
-     * @param c float number3
-     * @return maximum of 3 float numbers
-     */
-    public static Float maximumOfFloat(Float a, Float b, Float c) {
-        Float max = a;
-        if (b.compareTo(max) > 0)
-            max = b;
-        if (c.compareTo(max) > 0)
-            max = c;
-        return max;
-    }
-
-    /**
-     * @param a string1
-     * @param b string2
-     * @param c string3
-     * @return maximum of 3 strings
-     */
-    public static String maximumOfString(String a, String b, String c) {
-        String max = a;
-        if (b.compareTo(max) > 0)
-            max = b;
-        if (c.compareTo(max) > 0)
-            max = c;
-        return max;
     }
 
     @Override
     public int compareTo(Object o) {
         return 0;
     }
-
+    /**
+     * Returns the maximum of 3 comparable objects
+     */
     public static void main(String[] args) {
-        System.out.println(maximumOfIntegers(20, 30, 40));
-        System.out.println(maximumOfFloat(32.5f, 53.5f, 43.2f));
-        System.out.println(maximumOfString("apple", "banana", "peach"));
+        System.out.println(maximum(20, 30, 40));
+        System.out.println(maximum(22.5f, 53.5f, 32.2f));
+        System.out.println(maximum("apple", "banana", "peach"));
     }
-
 }
