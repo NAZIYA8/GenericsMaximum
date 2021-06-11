c572b197efc8488f2337f63eef8a1b7ea483f813
NAZIYA8 <syeda.naziya896@gmail.com>
2021-06-11 18:32:08 +0530

    Added: Find Maximum of 3 Integers

--- src/com/bridgelabz/Maximum.java
+++ src/com/bridgelabz/Maximum.java
@@ -1,7 +1,30 @@
 package com.bridgelabz;
 
-public class Maximum {
+import java.lang.Integer;
+
+public class Maximum implements Comparable {
+
+    /**
+     * @param x integer1
+     * @param y integer2
+     * @param z integer3
+     * @return maximum of 3 integers
+     */
+    public static Integer maximumOfIntegers(Integer x, Integer y, Integer z) {
+        Integer max = x;
+        if (y.compareTo(max) > 0)
+            max = y;
+        if (z.compareTo(max) > 0)
+            max = z;
+        return max;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        return 0;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Maximum Problems using Generics");
+        System.out.println(maximumOfIntegers(20, 30, 40));
     }
 }
