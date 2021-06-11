c8ccf25893ffaf09e4b2df6fdf735321623c759b
NAZIYA8 <syeda.naziya896@gmail.com>
2021-06-11 18:39:05 +0530

    Added: Find Maximum of 3 Strings

--- src/com/bridgelabz/Maximum.java
+++ src/com/bridgelabz/Maximum.java
@@ -35,6 +35,21 @@ public class Maximum implements Comparable {
         return max;
     }
 
+    /**
+     * @param a string1
+     * @param b string2
+     * @param c string3
+     * @return maximum of 3 strings
+     */
+    public static String maximumOfString(String a, String b, String c) {
+        String max = a;
+        if (b.compareTo(max) > 0)
+            max = b;
+        if (c.compareTo(max) > 0)
+            max = c;
+        return max;
+    }
+
     @Override
     public int compareTo(Object o) {
         return 0;
@@ -42,7 +57,8 @@ public class Maximum implements Comparable {
 
     public static void main(String[] args) {
         System.out.println(maximumOfIntegers(20, 30, 40));
-        System.out.println(maximumOfFloat(22.5f, 34.5f, 46.2f));
+        System.out.println(maximumOfFloat(32.5f, 53.5f, 43.2f));
+        System.out.println(maximumOfString("apple", "banana", "peach"));
     }
 
 }
