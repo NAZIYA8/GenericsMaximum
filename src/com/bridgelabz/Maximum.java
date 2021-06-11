44fcefa6211267ad0ec2f122cdf2b954b3dcef36
NAZIYA8 <syeda.naziya896@gmail.com>
2021-06-11 19:44:07 +0530

    Added: Extended max method to take more than 3 parameters

--- src/com/bridgelabz/Maximum.java
+++ src/com/bridgelabz/Maximum.java
@@ -1,36 +1,39 @@
 package com.bridgelabz;
 
 public class Maximum<T extends Comparable<T>> {
-    T x, y, z;
+    T[] inputArray;
 
-    public Maximum(T x, T y, T z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
+    public Maximum(T[] inputArray) {
+        this.inputArray = inputArray;
     }
 
     public T getMaximum() {
-        return Maximum.getMaximum(x, y, z);
+        return Maximum.getMaximum(inputArray);
     }
 
-    public static <T extends Comparable<T>> T getMaximum(T x, T y, T z) {
-        T max = x;
-
-        if (y.compareTo(max) > 0)
-            max = y;
-        if (z.compareTo(max) > 0)
-            max = z;
+    public static <T extends Comparable<T>> T getMaximum(T[] inputArray) {
+        T max = inputArray[0];
+        for (int i = 0; i < inputArray.length; i++) {
+            T element = inputArray[i];
+            if (element.compareTo(max) > 0) {
+                max = element;
+            }
+        }
         return max;
-
     }
+
 
     /**
-     * Returns the maximum of 3 comparable objects
+     * Returns the maximum of any number of parameters taken
      */
     public static void main(String[] args) {
-        System.out.println(getMaximum(20, 30, 40));
-        System.out.println(getMaximum(22.5f, 53.5f, 32.2f));
-        System.out.println(getMaximum("apple", "banana", "peach"));
+
+        Integer[] intArray = {10, 30, 50, 40, 60, 20};
+        Float[] floatArray = {10.5f, 23.4f, 35.7f, 47.2f};
+        String[] stringArray = {"apple", "banana", "cherry", "pear", "orange"};
+        System.out.println(new Maximum(intArray).getMaximum());
+        System.out.println(new Maximum(floatArray).getMaximum());
+        System.out.println(new Maximum(stringArray).getMaximum());
     }
 }
 
