4769afa853368b75bd49d15d40ec1b8b1320b3f3
NAZIYA8 <syeda.naziya896@gmail.com>
2021-06-11 18:46:12 +0530

    Refactor: create generic class to take 3 variables of generic type

--- src/com/bridgelabz/Maximum.java
+++ src/com/bridgelabz/Maximum.java
@@ -1,8 +1,19 @@
 package com.bridgelabz;
 
-public class Maximum implements Comparable {
+public class Maximum<T extends Comparable<T>> {
+    T x, y, z;
 
-    public static <T extends Comparable<T>> T maximum(T x, T y, T z) {
+    public Maximum(T x, T y, T z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public T getMaximum() {
+        return Maximum.getMaximum(x, y, z);
+    }
+
+    public static <T extends Comparable<T>> T getMaximum(T x, T y, T z) {
         T max = x;
 
         if (y.compareTo(max) > 0)
@@ -13,16 +24,13 @@ public class Maximum implements Comparable {
 
     }
 
-    @Override
-    public int compareTo(Object o) {
-        return 0;
-    }
     /**
      * Returns the maximum of 3 comparable objects
      */
     public static void main(String[] args) {
-        System.out.println(maximum(20, 30, 40));
-        System.out.println(maximum(22.5f, 53.5f, 32.2f));
-        System.out.println(maximum("apple", "banana", "peach"));
+        System.out.println(getMaximum(20, 30, 40));
+        System.out.println(getMaximum(22.5f, 53.5f, 32.2f));
+        System.out.println(getMaximum("apple", "banana", "peach"));
     }
 }
+
